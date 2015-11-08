3b72bccd89b474ff9c7a1183814b1089da494482
Frank Cash <cfcash@mtu.edu>
2015-11-08 02:07:13 -0500

    updates send code, senpai pls work

--- tx_frsk.py
+++ tx_frsk.py
@@ -2,50 +2,57 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: TX FRSK
-# Generated: Sat Nov  7 14:57:17 2015
-
+# Generated: Sun Nov  8 02:05:56 2015
 ##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
 
 from gnuradio import blocks
 from gnuradio import digital
 from gnuradio import eng_notation
-from gnuradio import filter
 from gnuradio import gr
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
+from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import osmosdr
+import time
+import wx
 
 
-class tx_frsk(gr.top_block):
+class tx_frsk(grc_wxgui.top_block_gui):
 
     def __init__(self):
-        gr.top_block.__init__(self, "TX FRSK")
+        grc_wxgui.top_block_gui.__init__(self, title="TX FRSK")
 
         ##################################################
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 48000
+        self.audio_rate = audio_rate = 250e3
+        self.audio_interp = audio_interp = 4
 
         ##################################################
         # Blocks
         ##################################################
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=201600,
-                decimation=230000,
-                taps=None,
-                fractional_bw=None,
-        )
         self.osmosdr_sink_0 = osmosdr.sink( args="numchan=" + str(1) + " " + "" )
         self.osmosdr_sink_0.set_sample_rate(samp_rate/2)
-        self.osmosdr_sink_0.set_center_freq(1250e6, 0)
+        self.osmosdr_sink_0.set_center_freq(415e6, 0)
         self.osmosdr_sink_0.set_freq_corr(0, 0)
         self.osmosdr_sink_0.set_gain(10, 0)
         self.osmosdr_sink_0.set_if_gain(20, 0)
         self.osmosdr_sink_0.set_bb_gain(20, 0)
         self.osmosdr_sink_0.set_antenna("", 0)
         self.osmosdr_sink_0.set_bandwidth(0, 0)
-
+          
         self.digital_psk_mod_0 = digital.psk.psk_mod(
           constellation_points=8,
           mod_code="gray",
@@ -55,14 +62,15 @@ class tx_frsk(gr.top_block):
           verbose=True,
           log=True,
           )
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, "info.dat", False)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((0.5, ))
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, "/Users/majora/Code/BasicSloth/out.dat", True)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_file_source_0, 0), (self.digital_psk_mod_0, 0))
-        self.connect((self.digital_psk_mod_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.osmosdr_sink_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.digital_psk_mod_0, 0))    
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.osmosdr_sink_0, 0))    
+        self.connect((self.digital_psk_mod_0, 0), (self.blocks_multiply_const_vxx_0, 0))    
 
 
     def get_samp_rate(self):
@@ -72,15 +80,22 @@ class tx_frsk(gr.top_block):
         self.samp_rate = samp_rate
         self.osmosdr_sink_0.set_sample_rate(self.samp_rate/2)
 
+    def get_audio_rate(self):
+        return self.audio_rate
+
+    def set_audio_rate(self, audio_rate):
+        self.audio_rate = audio_rate
+
+    def get_audio_interp(self):
+        return self.audio_interp
+
+    def set_audio_interp(self, audio_interp):
+        self.audio_interp = audio_interp
+
 
 if __name__ == '__main__':
     parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
     (options, args) = parser.parse_args()
     tb = tx_frsk()
-    tb.start()
-    try:
-        raw_input('Press Enter to quit: ')
-    except EOFError:
-        pass
-    tb.stop()
-    tb.wait()
+    tb.Start(True)
+    tb.Wait()
